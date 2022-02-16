e123b1c235fd2e471bc50f1be793fbcd22e7beec
Vadim-Maliarchuk <67371803+Vadim-Maliarchuk@users.noreply.github.com>
2022-02-16 17:10:23 +0300

    Delete cypress/integration/Factorial directory

--- cypress/integration/Factorial/factorial_URL.spec.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="Cypress" />
-
-it('Check URL', ()=> {
-    cy.visit('http://qainterview.pythonanywhere.com')
-   cy.url()
-         .should('eq', 'http://qainterview.pythonanywhere.com/')
-   
-   })
